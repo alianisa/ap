e86670a88ac90b717b7035a35a9f02021767505b
diego <diegosiuniube@gmail.com>
2018-02-12 17:04:29 -0200

    fixes

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/auth/SignPhoneFragment.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/controllers/auth/SignPhoneFragment.java
@@ -177,8 +177,7 @@ public class SignPhoneFragment extends BaseAuthFragment {
 
         phoneNumberEditText = (BackspaceKeyEditText) v.findViewById(R.id.tv_phone_number);
         phoneNumberEditText.setTextColor(ActorSDK.sharedActor().style.getTextPrimaryColor());
-
-        phoneNumberEditText.addTextChangedListener(Mask.telephoneMask(phoneNumberEditText));
+        phoneNumberEditText.setKeyListener(Mask.keylistenerNumber);
 
         phoneNumberEditText.setBackspaceListener(() -> {
             if (phoneNumberEditText.getText().length() == 0) {
