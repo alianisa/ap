73bda13201ed0538af9c161a5bce087770c2c012
Diego Silva <diegosiuniube@gmail.com>
2018-02-15 10:42:16 -0200

    fixes

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorSDK.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/sdk/ActorSDK.java
@@ -62,7 +62,15 @@ public class ActorSDK {
     /**
      * ActorSDKCreateListener default implementation
      */
-    protected ActorSDKCreateListener sdkCreateListener = ActorSDKCreateListener.stub;
+    protected ActorSDKCreateListener sdkCreateListener = new ActorSDKCreateListener() {
+        @Override
+        public void onCreateActor(Application application) {
+        }
+        @Override
+        public AndroidMessenger createMessenger(Application application, ConfigurationBuilder builder) {
+            return new AndroidMessenger(application, builder.build());
+        }
+    };
 
     //
     // SDK Objects
@@ -222,9 +230,6 @@ public class ActorSDK {
                 "6709b8b733a9f20a96b9091767ac19fd6a2a978ba0dccc85a9ac8f6b6560ac1a"
         };
     }
-
-    private BaseParser[] rpcParsers = new BaseParser[]{};
-    private BaseParser[] updateParsers = new BaseParser[]{};
 
     /**
      * Shared ActorSDK. Use this method to get instance of SDK for configuration and starting up
@@ -441,22 +446,6 @@ public class ActorSDK {
     public void setEndpoints(String[] endpoints) {
         this.endpoints = endpoints;
         this.trustedKeys = new String[0];
-    }
-
-    public BaseParser[] getRpcParsers() {
-        return rpcParsers;
-    }
-
-    public void setRpcParsers(BaseParser[] rpcParsers) {
-        this.rpcParsers = rpcParsers;
-    }
-
-    public BaseParser[] getUpdateParsers() {
-        return updateParsers;
-    }
-
-    public void setUpdateParsers(BaseParser[] updateParsers) {
-        this.updateParsers = updateParsers;
     }
 
     /**
